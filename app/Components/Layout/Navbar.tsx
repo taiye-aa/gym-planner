2c9f25bff6e616cff8cffc8f31a7d4df6b0aea25
Taiwo <taiwoaa04@gmail.com>
2026-03-04 22:19:09 +0100

    first commit

---
+++ app/Components/Layout/Navbar.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Link, Outlet } from 'react-router'
+import { Dumbbell } from 'lucide-react'
+import { Button } from '../ui/Button'
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+  const user = false;
+  return (
+    <div className='min-h-screen flex flex-col'>
+      <header className='fixed top-0 left-0 right-0 z-50 border-b border-border bg-background/80 backdrop-blur-md'>
+        <div className='max-w-6xl mx-auto px-6 h-16 flex items-center justify-between'>
+          <Link to="/"
+            className='flex items-center gap-2 text-foreground'>
+            <Dumbbell className='w-6 h-6 text-accent' />
+            <span className='font-semibold text-lg'>Gym Master</span>
+          </Link>
+          <nav>
+            {user ? (
+              <div className='flex items-center gap-2'>
+                <Link to="/Profile">
+                  <Button variant="ghost" size="sm">My Plan</Button>
+                </Link>
+
+              </div>
+            ) : (
+              <div className='flex items-center gap-2'>
+                <Link to="auth/sign-in">
+                  <Button variant="ghost" size="sm">Sign In</Button>
+                </Link>
+                <Link to="auth/sign-up">
+                  <Button size="sm">Sign Up</Button>
+                </Link>
+              </div>
+            )}
+
+          </nav>
+        </div>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  )
+}
+
+export default Navbar
--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,11 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    layout("Components/Layout/Navbar.tsx", [
+        index("routes/home.tsx"),
+        route('onboarding','routes/Onboarding.tsx'),
+        route('profile', 'routes/Profile.tsx'),
+        route('auth/:pathname', 'routes/Auth.tsx'),
+        route('account/:pathname','routes/Account.tsx'),
+    ])
+] satisfies RouteConfig;
